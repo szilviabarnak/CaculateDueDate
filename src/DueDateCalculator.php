ae6e8b21dd46a489d822062fe6b09f6e20ea5fd7
Barnák Szilvia <kovacsszilvia@gmail.com>
2021-03-07 12:03:27 +0100

    calculateDueDate resolved time számolás első része (hétvégefigyelés még hiányzik)

--- src/DueDateCalculator.php
+++ src/DueDateCalculator.php
@@ -1,7 +1,7 @@
 <?php
 final class DueDateCalculator{
     function isWorkingDay(DateTime $datetime){
-
+        //$day = $datetime->format('N');
         $julian_date = unixtojd($datetime->getTimestamp());
         $julian_dayofweek = jddayofweek($julian_date, 0);
 
@@ -11,13 +11,64 @@ final class DueDateCalculator{
             return false;
         }
     }
+
+    //17:01-re hibás!
     function isWorkingHours(DateTime $datetime){
-        if($this->isWorkingDay($datetime)){
+        if($this->isWorkingDay($datetime)){    
             $hour = $datetime->format('G');
-            if($hour >= 9 && $hour <= 17){
+            if($hour >= 9 && $hour <= 16){
                 return true;
             }else{
                 return false;
+            }
+        }
+
+    }
+    function howManyHoursLeft(DateTime $submitDateTime){
+        if($this->isWorkingHours($submitDateTime)){
+            $hour = (int)$submitDateTime->format('G');
+            return 16 - $hour;
+        }else{
+            return 0;
+        }
+    }
+    function calculateDueDate(DateTime $submitDateTime, int $turnaruondTime){
+        if($this->isWorkingHours($submitDateTime)){
+            $resolvedDateTime = new DateTime();
+            $howManyHoursLeft = $this->howManyHoursLeft($submitDateTime);
+            print_r($howManyHoursLeft);
+            if($turnaruondTime <= $howManyHoursLeft){
+                $turnaruondTime_DateInterval = new DateInterval('P0Y0M0DT'.$turnaruondTime.'H0M0S');
+                $resolvedDateTime = $submitDateTime->add($turnaruondTime_DateInterval);
+                return $resolvedDateTime;
+            }else{
+                $days = floor($turnaruondTime/8);
+                if($days == 0){
+                    //át kell billeneteni a napot
+                    $days++;
+                }
+                if($turnaruondTime%8 == 0){
+                    $turnaruondTime_DateInterval = new DateInterval('P0Y0M'.$days.'DT0H0M0S');
+                    $submitDateTime_Ymd = $submitDateTime;
+                }else{
+                    print("turnaruondTime:".$turnaruondTime." ");
+                    print("howManyHoursLeft:".$howManyHoursLeft." ");
+                    
+                    $remaining = ($turnaruondTime-$howManyHoursLeft)%8;
+                    //                 10       -     7
+                    print("remaining:".$remaining." ");
+                    print("days:".$days." ");
+                    
+                    $remaining--;
+                    $turnaruondTime_DateInterval = new DateInterval('P0Y0M'.$days.'DT'.$remaining.'H0M0S');
+                    $submitDateTime_Ymd = new DateTime($submitDateTime->format('Y-m-d 09:i:s'));
+                }
+                
+                
+                
+                $submitDateTime_Ymd = $submitDateTime_Ymd->add($turnaruondTime_DateInterval);
+                print_r($submitDateTime_Ymd);
+                return $submitDateTime_Ymd;
             }
             
         }else{
--- tests/DueDateCalculatorTest.php
+++ tests/DueDateCalculatorTest.php
@@ -66,7 +66,7 @@ final class DueDateCalculatorTest extends TestCase
 
     }
 
-    public static function IsWorkingdayTestData()
+    public static function isWorkingdayTestData()
     {
         return array(
             array(true, '2021-03-01 14:00:00'),
@@ -82,7 +82,7 @@ final class DueDateCalculatorTest extends TestCase
     }
 
     /**
-     * @dataProvider IsWorkingdayTestData 
+     * @dataProvider isWorkingdayTestData 
     */
     public function testIsWorkingDays_withParameters($result, $datetime){
         
@@ -124,6 +124,155 @@ final class DueDateCalculatorTest extends TestCase
         $isWorkingHours = $dueDateCalculator->isWorkingHours(new DateTime('2021-03-05 08:59:00'));
         //assert
         $this->assertEquals(false, $isWorkingHours);
+    }
+    
+    public function testIsWorkingHours_WhenDateIsWeekdayAndHourOutWorkinghours2_ShoulReturnFalse(){
+        //prepare
+        $dueDateCalculator = new DueDateCalculator();
+        //act
+        $isWorkingHours = $dueDateCalculator->isWorkingHours(new DateTime('2021-03-05 17:01:00'));
+        //assert
+        $this->assertEquals(false, $isWorkingHours);
+    }
+
+
+    public function testcalculateDueDate_WhenDateIsWeekday_ShoulReturnDateTime(){
+        //prepare
+        $dueDateCalculator = new DueDateCalculator();
+        //act
+        $resolvedDateTime = $dueDateCalculator->calculateDueDate(new DateTime('2021-03-05 13:00:00'), 3);
+
+        //assert
+        $this->assertEquals(new DateTime('2021-03-05 16:00:00'), $resolvedDateTime);
+    }
+    public static function calculateDueDateTestData_Workdays()
+    {
+        return array(
+            array('2021-03-09 13:59:00', 3 , '2021-03-09 16:59:00'),
+            array('2021-03-08 09:00:00', 2 , '2021-03-08 11:00:00')
+        );
+    }
+
+    /**
+     * @dataProvider calculateDueDateTestData_Workdays 
+    */
+    public function testCalculateDueDateWithParameters_WhenDateIsWeekday_ShoulReturnDateTime(string $submitDateTime, int $turnaruondTime, string $result){
+        //prepare
+        $dueDateCalculator = new DueDateCalculator();
+        //act
+        $resolvedDateTime = $dueDateCalculator->calculateDueDate(new DateTime($submitDateTime), $turnaruondTime);
+        
+        //assert
+        $this->assertEquals(new DateTime($result), $resolvedDateTime);
+    }
+    
+    public static function calculateDueDateTestData_Weekends()
+    {
+        return array(
+            array('2021-03-09 18:01:00', 3 , '2021-03-09 17:00:00'),
+            array('2021-03-06 09:00:00', 2 , '2021-03-06 11:00:00')
+        );
+    }
+
+    /**
+     * @dataProvider calculateDueDateTestData_Weekends 
+    */
+    public function testCalculateDueDateWithParameters_WhenDateIsWeekend_ShoulReturnFalse(string $submitDateTime, int $turnaruondTime, string $result){
+        //prepare
+        $dueDateCalculator = new DueDateCalculator();
+        //act
+        $resolvedDateTime = $dueDateCalculator->calculateDueDate(new DateTime($submitDateTime), $turnaruondTime);
+        
+        //assert
+        $this->assertEquals(false, $resolvedDateTime);
+    }
+
+
+    public function testHowManyHoursLeft_WhenNoWorkingTime_ShouldReturnZero(){
+        //prepare
+        $dueDateCalculator = new DueDateCalculator();
+        //act
+        $howManyHoursLeft = $dueDateCalculator->howManyHoursLeft(new DateTime('2021-03-05 18:59:00'));
+        
+        //assert
+        $this->assertEquals(0, $howManyHoursLeft);
+    }
+    
+    public function testcalculateDueDate_WhenDateIsWeekday_ResolvedDateToday_ShoulReturnDateTime(){
+        //prepare
+        $dueDateCalculator = new DueDateCalculator();
+        //act
+        $resolvedDateTime = $dueDateCalculator->calculateDueDate(new DateTime('2021-03-05 09:00:00'), 5);
+        //assert
+        $this->assertEquals(new DateTime('2021-03-05 14:00:00'), $resolvedDateTime);
+    }
+    public function testcalculateDueDate_WhenDateIsWeekday_ResolvedDateTomorrow_ShoulReturnDateTime(){
+        //prepare
+        $dueDateCalculator = new DueDateCalculator();
+        //act
+        $resolvedDateTime = $dueDateCalculator->calculateDueDate(new DateTime('2021-03-05 15:10:00'), 5);
+        //assert
+        $this->assertEquals(new DateTime('2021-03-06 12:10:00'), $resolvedDateTime);
+    }
+    
+    public function testcalculateDueDate_WhenDateIsWeekday_ResolvedDateTomorrow2_ShoulReturnDateTime(){
+        //prepare
+        $dueDateCalculator = new DueDateCalculator();
+        //act
+        $resolvedDateTime = $dueDateCalculator->calculateDueDate(new DateTime('2021-03-05 14:12:00'), 16);
+        //assert
+        $this->assertEquals(new DateTime('2021-03-07 14:12:00'), $resolvedDateTime);
+    }
+
+    public static function calculateDueDateTestData2_Workdays()
+    {
+        return array(
+            array('2021-03-02 09:59:00', 10 , '2021-03-03 11:59:00'),
+            array('2021-03-02 09:59:00', 18 , '2021-03-04 11:59:00'),
+            array('2021-03-09 13:59:00', 3 , '2021-03-09 16:59:00'),
+            array('2021-03-09 16:59:00', 3 , '2021-03-10 11:59:00'),
+            array('2021-03-08 09:00:00', 2 , '2021-03-08 11:00:00')
+        );
+    }
+
+    /**
+     * @dataProvider calculateDueDateTestData2_Workdays 
+    */
+    public function testCalculateDueDateWithParameters_WhenDateIsWeekday_ResolvedDateTomorrow2_ShoulReturnDateTime(string $submitDateTime, int $turnaruondTime, string $result){
+        //prepare
+        $dueDateCalculator = new DueDateCalculator();
+        //act
+        $resolvedDateTime = $dueDateCalculator->calculateDueDate(new DateTime($submitDateTime), $turnaruondTime);
+        
+        //assert
+        $this->assertEquals(new DateTime($result), $resolvedDateTime);
+    }
+
+
+
+    public function testHowManyHoursLeft_WhenTimeBetween917_ShouldReturnInt(){
+        //prepare
+        $dueDateCalculator = new DueDateCalculator();
+        //act
+        $howManyHoursLeft = $dueDateCalculator->howManyHoursLeft(new DateTime('2021-03-05 09:00:00'));
+        //assert
+        $this->assertEquals(7, $howManyHoursLeft);
+    }
+    public function testHowManyHoursLeft_WhenTimeGreaterThan16_ShouldReturnZero(){
+        //prepare
+        $dueDateCalculator = new DueDateCalculator();
+        //act
+        $howManyHoursLeft = $dueDateCalculator->howManyHoursLeft(new DateTime('2021-03-05 19:00:00'));
+        //assert
+        $this->assertEquals(0, $howManyHoursLeft);
+    }
+    public function testHowManyHoursLeft_WhenTimeLessThan9_ShouldReturnZero(){
+        //prepare
+        $dueDateCalculator = new DueDateCalculator();
+        //act
+        $howManyHoursLeft = $dueDateCalculator->howManyHoursLeft(new DateTime('2021-03-05 08:00:00'));
+        //assert
+        $this->assertEquals(0, $howManyHoursLeft);
     }
 }
 ?>
